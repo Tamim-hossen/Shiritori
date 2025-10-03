4f573b06d72609a8557e069ac6b384fd636511c2
Tamim <tamimhossen003@gmail.com>
2025-10-03 22:46:27 +0600

    initial commit

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { Toaster } from "react-hot-toast";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -23,7 +23,9 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Toaster/>
       </body>
+      
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,103 +1,186 @@
-import Image from "next/image";
+'use client'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import toast from 'react-hot-toast'
+function page() {
+  const [usedwords, setUsedWords] = useState([])
+  const [lastword, setLastWord] = useState('')
+  const [currentWord, setCurrentWord] = useState('')
+  const [currentplayer, setCurrentPlayer] = useState('player1')
+  const [player1score, setPlayer1score] = useState(0)
+  const [player2score, setPlayer2score] = useState(0)
+  const [timer, setTimer] = useState(60)
+  const [lastLetter, setLastLetter] = useState('')
 
-export default function Home() {
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      setTimer((prev) => prev - 1)
+      if (timer === 0) {
+        setTimer(60)
+        if (currentplayer === 'player1') {
+          if (player1score !== 0) {
+            setPlayer1score((prev) => prev - 1)
+          }
+
+          setCurrentPlayer('player2')
+        }
+        else {
+          if (player2score !== 0) {
+            setPlayer2score((prev) => prev - 1)
+          }
+          setCurrentPlayer('player1')
+
+        }
+      }
+    }, 1000)
+
+    return () => clearInterval(intervalID);
+  }, [timer])
+
+
+  const lastwordcheck = (lastword, currentword) => {
+    const lastsplit = lastword.split("").reverse()
+    const currentsplit = currentWord.split("")
+    const lastletter = lastsplit[0].toLowerCase()
+    const firstletter = currentsplit[0].toLowerCase()
+
+    if (lastletter === firstletter) {
+      return true
+    }
+    else {
+      return false
+    }
+
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    if (currentWord.length > 3) {
+
+      if (usedwords.includes(`${currentWord}`)) {
+        toast.error('Word Already used')
+      }
+
+      else {
+        const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${currentWord}`
+        const isaword = await fetch(url)
+        if (isaword.ok) {
+          if (usedwords.length == 0) {
+            setUsedWords([...usedwords, currentWord])
+            const splitword = currentWord.split("").reverse()
+            setLastWord(currentWord)
+            setCurrentWord('')
+            setLastLetter(splitword[0])
+            setTimer(60)
+
+            if (currentplayer === 'player1') {
+              setPlayer1score((prev) => prev + (1 * timer / 10))
+              setCurrentPlayer('player2')
+            }
+            else {
+              setCurrentPlayer('player1')
+              setPlayer2score((prev) => prev + (1 * timer / 10))
+            }
+          }
+          else {
+            const lettercheck = lastwordcheck(lastword, currentWord)
+
+
+            if (lettercheck) {
+              setUsedWords([currentWord, ...usedwords])
+              setLastWord(currentWord)
+              const splitword = currentWord.split("").reverse()
+              setCurrentWord('')
+              setLastLetter(splitword[0])
+              setTimer(60)
+              if (currentplayer === 'player1') {
+                setPlayer1score((prev) => prev + (1 * timer / 10))
+                setCurrentPlayer('player2')
+              }
+              else {
+                setCurrentPlayer('player1')
+                setPlayer2score((prev) => prev + (1 * timer / 10))
+              }
+            }
+            else {
+              toast.error('First letter does not match the previous words last letter')
+            }
+          }
+        }
+        else {
+          toast.error('Wrong Word')
+          if (currentplayer === 'player1') {
+            setPlayer1score((prev) => prev - 1)
+            setCurrentPlayer('player2')
+            setCurrentWord('')
+          }
+          else {
+            setPlayer2score((prev) => prev - 1)
+            setCurrentPlayer('player1')
+            setCurrentWord('')
+          }
+        }
+      }
+    }
+    else {
+      toast.error('Minimum 4 words')
+    }
+  }
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className='p-10 flex flex-col gap-10 justify-center itms-center w-full'>
+      <label className='text-4xl text-center'>Turn : {currentplayer == 'player1' ? 'player 1' : "player 2"}</label>
+      <label className='text-center text-2xl'>Timer : {timer}</label>
+      <label className='text-center text-2xl'>Next Word Starts with : {usedwords.length == 0 ? 'Any' : lastLetter}</label>
+      <div className='flex flex-row gap-20 items-center justify-center'>
+        <div className={`flex flex-col gap-5 justify-center items-center  p-10 rounded-md ${currentplayer === 'player1' ? 'bg-amber-100' : 'bg-transparent'} transition-bg duration-100`}>
+          <p className='text-xl'>Player 1</p>
+          <form onSubmit={currentplayer === 'player1' ? handleSubmit : (e) => { e.preventDefault(); toast.error(`Player 2's Turn`) }} className='flex flex-row gap-2'>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <div className='flex flex-row gap-5 justify-center items-center'>
+              <label>Enter Your Text</label>
+              <input type='text' value={currentplayer === 'player1' ? currentWord : lastword} onChange={(e) => { currentplayer === 'player1' ? setCurrentWord(e.target.value.toLowerCase()) : '' }} className={`border-1 border-black p-2 ${currentplayer === 'player1' ? 'focus:ring-2 h-10 ' : 'disabled h-5'} transition-all`} />
+            </div>
+            <button type='submit' className={`  ${currentplayer === 'player1' ? 'px-5 py-2 bg-amber-300 hover:bg-amber-400 cursor-pointer' : 'cursor-not-allowed py-1 px-2 bg-red-100'}`}> submit</button>
+
+          </form>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className={`flex flex-col gap-5 justify-center items-center  p-10 rounded-md ${currentplayer === 'player2' ? 'bg-amber-100' : 'bg-transparent'} transition-bg duration-100`}>
+          <p className='text-xl'>Player 2</p>
+          <form onSubmit={currentplayer === 'player2' ? handleSubmit : (e) => { e.preventDefault(); toast.error(`Player 1's Turn`) }} className='flex flex-row gap-2 justify-center items-center'>
+
+            <div className='flex flex-row gap-5 justify-center items-center' >
+              <label>Enter Your Text</label>
+              <input type='text' value={currentplayer === 'player2' ? currentWord : lastword} onChange={(e) => { currentplayer === 'player2' ? setCurrentWord(e.target.value.toLowerCase()) : '' }} className={`border-1 border-black p-2  ${currentplayer === 'player2' ? 'focus h-10' : 'disabled h-5 cursor-not-allowed'} transition-all`} />
+            </div>
+            <button type='submit' className={`  ${currentplayer === 'player2' ? 'px-5 py-2 bg-amber-300 hover:bg-amber-400 cursor-pointer' : 'cursor-not-allowed py-1 px-2 bg-red-100'}`}> submit</button>
+
+          </form>
+        </div>
+      </div>
+      <div className='w-full flex flex-col justify-center'>
+        <p className='text-center'>Player 1 score : {player1score}</p>
+        <p className='text-center'>Player 2 score : {player2score}</p>
+      </div>
+
+
+      <div className='pt-20'>
+        <p className='text-center text-2xl pb-5'>Used Words</p>
+        {
+          usedwords.map((word, index) => {
+            return (
+              <div key={index} className='flex flex-row gap-2 border-b-1 pb-1 border-gray-400'>
+                <p>{index + 1}.</p>
+                <p className=''>{word}</p>
+              </div>
+
+            )
+
+          })
+        }
+      </div>
     </div>
-  );
+  )
 }
+
+export default page
